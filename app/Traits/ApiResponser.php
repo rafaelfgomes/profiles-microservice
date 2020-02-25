dd0b8b59bf4b749ecde53b84fd59276aef65e583
Rafael Gomes <faelfg@gmail.com>
2020-02-25 18:02:26 -0300

    Created trait for api responses

---
+++ app/Traits/ApiResponser.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\Response;
+
+trait ApiResponser
+{
+    /**
+   *
+   * Success Response
+   *
+   * @param string|array $data
+   * @param int $code
+   * @return Illuminate\Http\JsonResponse
+   *
+   *
+   */
+  public function successResponse($data, $code = Response::HTTP_OK)
+  {
+
+    return response()->json([ 'data' => $data ], $code);
+
+  }
+
+  /**
+   *
+   * Success Response
+   *
+   * @param string|array $data
+   * @param int $code
+   * @return Illuminate\Http\JsonResponse
+   *
+   */
+  public function errorResponse($message, $code)
+  {
+
+    return response()->json([ 'error' => $message, 'code' => $code ], $code);
+
+  }
+}
